1f6f6ec7ea6f14b13f7a840f77d90aa4e1f98f6b
Dmytro Hopko <gopkodimon@gmail.com>
2021-04-16 23:30:40 +0300

    ready advanced Homework lesson 6

--- js/script.js
+++ js/script.js
@@ -1,32 +1,46 @@
 "use strict";
 
-
 function numbersGame() {
     let setNum = 48;
-    
-  function gameLogic() {
-    let userNum = prompt("Угадай число от 1 до 100");
-    if (userNum === null) {
-        alert("Игра окончена");
-    }else if (isNaN(userNum)) {
-      alert("Введи число!");
-      gameLogic();
-    } else if (userNum == setNum) {
-      alert("Поздравляю, Вы угадали!!!");
-    } else if (setNum < userNum) {
-      alert("Загаданное число больше");
-      gameLogic();
-    } else if (setNum > userNum) {
-      alert("Загаданное число меньше");
-      gameLogic();
+    let n = 3;
+
+    function gameLogic() {
+        let userNum = prompt("Угадай число от 1 до 100");
+        if (n === 1 && setNum != userNum) {
+            let answ = confirm("Попытки закончились, хотите сыграть еще?");
+            if (answ) {
+                n = 3;
+                gameLogic();
+            } else {
+                return null;
+            }
+        } else if (userNum === null) {
+            alert("Игра окончена");
+        } else if (isNaN(userNum)) {
+            alert("Введи число!");
+            gameLogic();
+        } else if (userNum == setNum) {
+            let answ2 = confirm(
+                "Поздравляю, Вы угадали!!! Хотели бы сыграть еще?"
+            );
+            if (answ2) {
+                n = 3;
+                gameLogic();
+            } else {
+                return null;
+            }
+        } else if (setNum < userNum) {
+            n--;
+            alert("Загаданное число больше, осталось попыток " + n);
+            gameLogic();
+        } else if (setNum > userNum) {
+            n--;
+            alert("Загаданное число меньше, осталось попыток " + n);
+            gameLogic();
+        }
     }
-  }
-  return gameLogic;
+    return gameLogic;
 }
 
 const startGame = numbersGame();
-startGame(); 
-
-
-
-
+startGame();
