c4a939a9c2394039c76058f00ec8e5340c714229
Dmytro Hopko <gopkodimon@gmail.com>
2021-04-16 22:33:21 +0300

    ready homework lesson 6

---
+++ js/script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+
+function numbersGame() {
+    let setNum = 48;
+    
+  function gameLogic() {
+    let userNum = prompt("Угадай число от 1 до 100");
+    if (userNum === null) {
+        alert("Игра окончена");
+    }else if (isNaN(userNum)) {
+      alert("Введи число!");
+      gameLogic();
+    } else if (userNum == setNum) {
+      alert("Поздравляю, Вы угадали!!!");
+    } else if (setNum < userNum) {
+      alert("Загаданное число больше");
+      gameLogic();
+    } else if (setNum > userNum) {
+      alert("Загаданное число меньше");
+      gameLogic();
+    }
+  }
+  return gameLogic;
+}
+
+const startGame = numbersGame();
+startGame(); 
